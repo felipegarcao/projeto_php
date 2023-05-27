b216c392ed6736742993b961a2ddf639c4f97d90
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2023-05-27 14:23:40 -0300

    categories

--- App/Views/category/index.php
+++ App/Views/category/index.php
@@ -1,33 +1,50 @@
 <div class="container">
     <div class="row">
         <br />
-        <div class="col-md-2 mt-4">
+        <div class="col-md-2 mt-5">
             <a href="http://<?php echo APP_HOST; ?>/category/cadastro" class="buttonSubmit"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Adicionar</a>
         </div>
         <div class="col-md-12">
             <hr>
-            
-            <?php if($Sessao::retornaMensagem()){ ?>
+
+            <?php if ($Sessao::retornaMensagem()) { ?>
                 <div class="alert alert-warning" role="alert">
                     <a href="" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                    <?= $Sessao::retornaMensagem() ?> 
+                    <?= $Sessao::retornaMensagem() ?>
                     <br>
                 </div>
-            <?php } ?>            
+            <?php } ?>
 
-            <?php if(!count($viewVar['listCategory'])){ ?>
+            <?php if (!count($viewVar['listCategory'])) { ?>
                 <div class="alert alert-info" role="alert">Nenhum fornecedor encontrado</div>
-            <?php } else { ?>      
-                
-                <div class="row mx-auto">
-                <div class="col-md-3" style="width: 200px;">
-                    
+            <?php } else { ?>
+
+                <div class="containerCategory">
+                    <div class="col-md-3 m-1 card-category">
+                        <strong>Comedia</strong>
+                        <div class="d-flex justify-content-end gap-3 align-items-end mt-5">
+                            <a href="" class="editar">
+                                <img src="http://<?php echo APP_HOST; ?>/public/icons/pencil.png" alt="editar" />
+                            </a>
+                            <a href="" class="excluir">
+                                <img src="http://<?php echo APP_HOST; ?>/public/icons/x.png" alt="apagar" />
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-3 m-1 card-category">
+                        <strong>Comedia</strong>
+                        <div class="d-flex justify-content-end gap-3 align-items-end mt-5">
+                            <a href="" class="editar">
+                                <img src="http://<?php echo APP_HOST; ?>/public/icons/pencil.png" alt="editar" />
+                            </a>
+                            <a href="" class="excluir">
+                                <img src="http://<?php echo APP_HOST; ?>/public/icons/x.png" alt="apagar" />
+                            </a>
+                        </div>
+                    </div>
+
                 </div>
-                <div class="col-md-3" style="width: 200px;">TESTE</div>
-                <div class="col-md-3" style="width: 200px;">TESTE</div>
-                <div class="col-md-3" style="width: 200px;">TESTE</div>
-                </div>
-                
+
 
 
                 <!-- <div class="table-responsive">
@@ -36,7 +53,7 @@
                             <td class="info">Nome</td>
                             <td class="info text-center">Ação</td>
                         </tr>
-                        <?php foreach($viewVar['listCategory'] as $category) { ?>
+                        <?php foreach ($viewVar['listCategory'] as $category) { ?>
                             <tr>
                                 <td><?= $category->getName() ?></td>
                                 <td style="width:15%">
