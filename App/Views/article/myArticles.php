70ea560e49a44e1c8ef8c5c3215a6bb424d1bb65
Nicole <118059749+NisckzAlves@users.noreply.github.com>
2023-06-06 11:43:07 -0300

    [FIX] filtro meus posts

--- App/Views/article/myArticles.php
+++ App/Views/article/myArticles.php
@@ -1,172 +1,81 @@
-<?php if (is_null($viewVar['articleExibition'])) { ?>
-<div class="alert alert-info" role="alert">Nenhum artigo postado :c</div>
-<?php } else {
-    if ($Sessao::retornaMensagem()) { ?>
-<div class="alert alert-warning" role="alert">
-  <a href="" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-  <?= $Sessao::retornaMensagem() ?>
-  <br>
-</div>
-<?php } ?>
-<div class="container containerSolicitacao">
-  <h1>Meus Posts</h1>
-
-  <form class="d-flex align-items-center gap-2 justify-content-center mb-4">
-<select class="select" name="idCategory" id="idCategory" required>
-        <?php foreach ($viewVar['listCategory'] as $category) { ?>
-        <option value="<?= $category->getIdCategory() ?>"><?= $category->getName() ?></option>
-        <?php } ?>
-      </select>
-      <button class="buttonSubmit" style="height: 33px">
-      <img src="http://<?php echo APP_HOST; ?>/public/icons/search.png" alt="buscar" />
-    </button>
-
-</form>
-
-  <?php foreach ($viewVar['articleExibition'] as $article) {
-            if ($article->getStatus() == "Aproved") {
-        ?>
-  <a href="http://<?= APP_HOST ?>/article/detalhes/<?= $article->getIdArticle() ?>">
-    <div class="cardContainer">
-      <div class="flag aceito">
-        <span>Aprovado</span>
-      </div>
-      <header>
-        <strong class="text-center"><?= $article->getTitle() ?></strong>
-        <div class="detalhesPostInfo">
-          <ul>
-            <li>
-              <img src="http://<?php echo APP_HOST; ?>/public/icons/calendar.png" alt="calendario" />
-              <time><?= $article->getCreatedAt()->format('d/m/Y') ?></time>
-            </li>
-            <li>
-              <img src="http://<?php echo APP_HOST; ?>/public/icons/user.png" alt="user" />
-              <?= $viewVar['user']->getName() ?>
-            </li>
-          </ul>
+<body>
+  <main class="container">
+    <?php if (is_null($viewVar['articleExibition'])) { ?>
+      <div class="alert alert-info" role="alert">Nenhum artigo postado :c</div>
+    <?php } else {
+      if ($Sessao::retornaMensagem()) { ?>
+        <div class="alert alert-warning" role="alert">
+          <a href="" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+          <?= $Sessao::retornaMensagem() ?>
+          <br>
         </div>
-      </header>
+      <?php } ?>
+      <div class="container containerSolicitacao">
+        <h1>Meus Posts</h1>
 
-      <p>
-        <?= $article->getResume() ?>
-      </p>
-    </div>
-  </a>
-  <?php
-            } elseif ($article->getStatus() == "Pending") {
+        <form class="d-flex align-items-center gap-2 justify-content-center mb-4" method="GET">
+          </select>
+          <select class="select" name="status" id="status" onchange="this.form.submit()">
+            <option value="">Todos</option>
+            <option value="Aproved" <?= (isset($_GET['status']) && $_GET['status'] == 'Aproved') ? 'selected' : '' ?>>Aprovados</option>
+            <option value="Pending" <?= (isset($_GET['status']) && $_GET['status'] == 'Pending') ? 'selected' : '' ?>>Pendentes</option>
+            <option value="Denied" <?= (isset($_GET['status']) && $_GET['status'] == 'Denied') ? 'selected' : '' ?>>Negados</option>
+          </select>
+        </form>
+
+        <?php foreach ($viewVar['articleExibition'] as $article) {
+          $statusFilter = isset($_GET['status']) ? $_GET['status'] : '';
+
+          if ($statusFilter === '' || $article->getStatus() === $statusFilter) {
             ?>
-  <div class="cardContainer" style="position: relative">
-    <div class="flag pendente">
-      <span>Pendente</span>
-    </div>
-    <button class="position-absolute btn" style="right: 20px; top: 10px;">
-     <a href="http://<?= APP_HOST ?>/article/edit/<?= $article->getIdArticle() ?>">
-      <img src="http://<?php echo APP_HOST; ?>/public/icons/pencil.png" alt="editar" />
-      </a>
-    </button>
-    <a href="http://<?= APP_HOST ?>/article/detalhes/<?= $article->getIdArticle() ?>">
-      <header>
-        <strong class="text-center"><?= $article->getTitle() ?></strong>
-        <div class="detalhesPostInfo">
-          <ul>
-            <li>
-              <img src="http://<?php echo APP_HOST; ?>/public/icons/calendar.png" alt="calendario" />
-              <time><?= $article->getCreatedAt()->format('d/m/Y') ?></time>
-            </li>
-            <li>
-              <img src="http://<?php echo APP_HOST; ?>/public/icons/user.png" alt="user" />
-              <?= $viewVar['user']->getName() ?>
-            </li>
-          </ul>
-        </div>
-      </header>
-    </a>
-    <p>
-      <?= $article->getResume() ?>
-    </p>
-  </div>
-  <?php
-            } elseif ($article->getStatus() == "Denied") {
-            ?>
-  <div class="cardContainer" style="position: relative;">
-    <div class="flag negado">
-      <span>Negado</span>
-    </div>
-    <button class="position-absolute btn" style="right: 20px; top: 10px;" data-bs-toggle="modal"
-      data-bs-target="#exampleModal<?= $article->getIdArticle() ?>">
-      <img src="http://<?php echo APP_HOST; ?>/public/icons/warning.png" alt="visualizar justificativa de negação" />
-    </button>
+            <a href="http://<?= APP_HOST ?>/article/detalhes/<?= $article->getIdArticle() ?>">
+              <div class="cardContainer">
+                <?php if ($article->getStatus() == "Aproved") { ?>
+                  <div class="flag aceito">
+                    <span>Aprovado</span>
+                  </div>
+                <?php } elseif ($article->getStatus() == "Pending") { ?>
+                  <div class="flag pendente">
+                    <span>Pendente</span>
+                  </div>
+                  <button class="position-absolute btn" style="right: 20px; top: 10px;">
+                    <a href="http://<?= APP_HOST ?>/article/edit/<?= $article->getIdArticle() ?>">
+                      <img src="http://<?php echo APP_HOST; ?>/public/icons/pencil.png" alt="editar" />
+                    </a>
+                  </button>
+                <?php } elseif ($article->getStatus() == "Denied") { ?>
+                  <div class="flag negado">
+                    <span>Negado</span>
+                  </div>
+                  <button class="position-absolute btn" style="right: 20px; top: 10px;" data-bs-toggle="modal" data-bs-target="#exampleModal<?= $article->getIdArticle() ?>">
+                    <img src="http://<?php echo APP_HOST; ?>/public/icons/warning.png" alt="visualizar justificativa de negação" />
+                  </button>
+                <?php } ?>
 
-    <a href="http://<?= APP_HOST ?>/article/detalhes/<?= $article->getIdArticle() ?>">
-      <header>
-        <strong class="text-center"><?= $article->getTitle() ?></strong>
-        <div class="detalhesPostInfo">
-          <ul>
-            <li>
-              <img src="http://<?php echo APP_HOST; ?>/public/icons/calendar.png" alt="calendario" />
-              <time><?= $article->getCreatedAt()->format('d/m/Y') ?></time>
-            </li>
-            <li>
-              <img src="http://<?php echo APP_HOST; ?>/public/icons/user.png" alt="user" />
-              <?= $viewVar['user']->getName() ?>
+                <header>
+                  <strong class="text-center"><?= $article->getTitle() ?></strong>
+                  <div class="detalhesPostInfo">
+                    <ul>
+                      <li>
+                        <img src="http://<?php echo APP_HOST; ?>/public/icons/calendar.png" alt="calendario" />
+                        <time><?= $article->getCreatedAt()->format('d/m/Y') ?></time>
+                      </li>
+                      <li>
+                        <img src="http://<?php echo APP_HOST; ?>/public/icons/user.png" alt="user" />
+                        <?= $viewVar['user']->getName() ?>
+                      </li>
+                    </ul>
+                  </div>
+                </header>
 
-            </li>
-          </ul>
-        </div>
-      </header>
-    </a>
-
-    <p>
-      <?= $article->getResume() ?>
-    </p>
-  </div>
-
-
-  <div class="modal fade" id="exampleModal<?= $article->getIdArticle() ?>" tabindex="-1"
-    aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-lg">
-      <div class="modal-content">
-        <div class="modal-header">
-          <p class="modal-title fs-5" id="exampleModalLabel">Justificativa</p>
-          <button type="button" class="btn" data-bs-dismiss="modal" aria-label="Close">
-            <img src="http://<?php echo APP_HOST; ?>/public/icons/x.png" alt="negar postagem" />
-          </button>
-        </div>
-        <div class="modal-body" style="padding: 2rem;">
-          <div class="d-flex gap-3">
-
-            <div>
-              <img src="https://i.gifer.com/SU9.gif"
-                alt="visualizar justificativa de negação" />
-            </div>
-            <div class="detalhesPostInfo">
-              <ul>
-                <li>
-                  <img src="http://<?php echo APP_HOST; ?>/public/icons/calendar.png" alt="calendario" />
-                  <time><?= $article->getCreatedAt()->format('d/m/Y') ?></time>
-                </li>
-                <li>
-                  <img src="http://<?php echo APP_HOST; ?>/public/icons/user.png" alt="user" />
-                  <?= $viewVar['user']->getName() ?>
-                </li>
-              </ul>
-              <p style="margin-left: 2rem; text-align: justify; color: #D7D7D7; font-size: 14px; line-height: 2;">
-                <?= $article->getFeedback() ?>
-              </p>
-            </div>
-
-
-          </div>
-
-        </div>
-
+                <p>
+                  <?= $article->getResume() ?>
+                </p>
+              </div>
+            </a>
+          <?php }
+        } ?>
       </div>
-    </div>
-  </div>
-  <?php
-            }
-        }
-    }
-    ?>
-
-    
+    <?php } ?>
+  </main>
+</body>
