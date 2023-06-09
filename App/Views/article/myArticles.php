f3600d14cdc5f114887d1528a79b5479ff85ad90
Nicole <118059749+NisckzAlves@users.noreply.github.com>
2023-06-09 20:13:54 -0300

    [FIX] modal dos negados em meus posts não estava sendo excibida

--- App/Views/article/myArticles.php
+++ App/Views/article/myArticles.php
@@ -14,7 +14,6 @@
         <h1>Meus Posts</h1>
 
         <form class="d-flex align-items-center gap-2 justify-content-center mb-4" method="GET">
-          </select>
           <select class="select" name="status" id="status" onchange="this.form.submit()">
             <option value="">Todos</option>
             <option value="Aproved" <?= (isset($_GET['status']) && $_GET['status'] == 'Aproved') ? 'selected' : '' ?>>Aprovados</option>
@@ -28,32 +27,72 @@
 
           if ($statusFilter === '' || $article->getStatus() === $statusFilter) {
             ?>
-            <a href="http://<?= APP_HOST ?>/article/detalhes/<?= $article->getIdArticle() ?>">
-              <div class="cardContainer">
-                <?php if ($article->getStatus() == "Aproved") { ?>
-                  <div class="flag aceito">
-                    <span>Aprovado</span>
-                  </div>
-                <?php } elseif ($article->getStatus() == "Pending") { ?>
-                  <div class="flag pendente">
-                    <span>Pendente</span>
-                  </div>
-                  <button class="position-absolute btn" style="right: 20px; top: 10px;">
-                    <a href="http://<?= APP_HOST ?>/article/edit/<?= $article->getIdArticle() ?>">
-                      <img src="http://<?php echo APP_HOST; ?>/public/icons/pencil.png" alt="editar" />
-                    </a>
-                  </button>
-                <?php } elseif ($article->getStatus() == "Denied") { ?>
-                  <div class="flag negado">
-                    <span>Negado</span>
-                  </div>
-                  <button class="position-absolute btn" style="right: 20px; top: 10px;" data-bs-toggle="modal" data-bs-target="#exampleModal<?= $article->getIdArticle() ?>">
-                    <img src="http://<?php echo APP_HOST; ?>/public/icons/warning.png" alt="visualizar justificativa de negação" />
-                  </button>
-                <?php } ?>
+            <div class="cardContainer">
+              <?php if ($article->getStatus() == "Aproved") { ?>
+                <div class="flag aceito">
+                  <span>Aprovado</span>
+                </div>
+              <?php } elseif ($article->getStatus() == "Pending") { ?>
+                <div class="flag pendente">
+                  <span>Pendente</span>
+                </div>
+                <button class="position-absolute btn" style="right: 20px; top: 10px;">
+                  <a href="http://<?= APP_HOST ?>/article/edit/<?= $article->getIdArticle() ?>">
+                    <img src="http://<?php echo APP_HOST; ?>/public/icons/pencil.png" alt="editar" />
+                  </a>
+                </button>
+              <?php } elseif ($article->getStatus() == "Denied") { ?>
+                <div class="flag negado">
+                  <span>Negado</span>
+                </div>
+                <button type="button" class="position-absolute btn" style="right: 20px; top: 10px;" data-bs-toggle="modal" data-bs-target="#exampleModal<?= $article->getIdArticle() ?>">
+                  <img src="http://<?php echo APP_HOST; ?>/public/icons/warning.png" alt="visualizar justificativa de negação" />
+                </button>
+              <?php } ?>
 
-                <header>
+              <header>
+                <a href="http://<?= APP_HOST ?>/article/detalhes/<?= $article->getIdArticle() ?>">
                   <strong class="text-center"><?= $article->getTitle() ?></strong>
+                </a>
+                <div class="detalhesPostInfo">
+                  <ul>
+                    <li>
+                      <img src="http://<?php echo APP_HOST; ?>/public/icons/calendar.png" alt="calendario" />
+                      <time><?= $article->getCreatedAt()->format('d/m/Y') ?></time>
+                    </li>
+                    <li>
+                      <img src="http://<?php echo APP_HOST; ?>/public/icons/user.png" alt="user" />
+                      <?= $viewVar['user']->getName() ?>
+                    </li>
+                  </ul>
+                </div>
+              </header>
+
+              <p>
+                <?= $article->getResume() ?>
+              </p>
+            </div>
+          <?php }
+        } ?>
+      </div>
+    <?php } ?>
+
+    <?php foreach ($viewVar['articleExibition'] as $article) {
+      if ($article->getStatus() == "Denied") { ?>
+        <div class="modal fade" id="exampleModal<?= $article->getIdArticle() ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered modal-lg">
+            <div class="modal-content">
+              <div class="modal-header">
+                <p class="modal-title fs-5" id="exampleModalLabel">Justificativa</p>
+                <button type="button" class="btn" data-bs-dismiss="modal" aria-label="Close">
+                  <img src="http://<?php echo APP_HOST; ?>/public/icons/x.png" alt="negar postagem" />
+                </button>
+              </div>
+              <div class="modal-body" style="padding: 2rem;">
+                <div class="d-flex gap-3">
+                  <div>
+                    <img src="http://<?php echo APP_HOST; ?>/public/icons/warning-big.png" alt="visualizar justificativa de negação" />
+                  </div>
                   <div class="detalhesPostInfo">
                     <ul>
                       <li>
@@ -65,17 +104,19 @@
                         <?= $viewVar['user']->getName() ?>
                       </li>
                     </ul>
+                    <p style="margin-left: 2rem; text-align: justify; color: #D7D7D7; font-size: 14px; line-height: 2;">
+                      <?= $article->getFeedback() ?>
+                    </p>
                   </div>
-                </header>
-
-                <p>
-                  <?= $article->getResume() ?>
-                </p>
+                </div>
               </div>
-            </a>
-          <?php }
-        } ?>
-      </div>
-    <?php } ?>
+              <div class="modal-footer">
+                <button type="button" class="btn negado" style="color: #FF57B2;" data-bs-dismiss="modal">Fechar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+    <?php }
+    } ?>
   </main>
 </body>
